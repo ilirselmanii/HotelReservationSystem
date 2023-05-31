07f05ac0b93df1e5b9dd145638cf857c30fe0853
ardian-sa <ardiansallauka1@gmail.com>
2023-05-31 18:31:47 +0200

    about

---
+++ about.php
@@ -0,0 +1,320 @@
+
+
+<!DOCTYPE html>
+<html>
+
+
+<head>
+<meta charset="UTF-8">
+<title>Relax Hostel</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Linking Bootstrap css file -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- Linking Main Css file -->
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/flatpickr.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+	
+	<link rel="stylesheet" type="text/css" href="css/popup.php">
+</head>
+
+
+<body>
+
+	<div class="wrapper">
+
+
+	<header class="abs">
+			<div class="top-bar">
+				<div class="container">
+					<div class="con-links">
+						<ul>
+								<li><i class="fa fa-phone" aria-hidden="true"></i> (+383) 49 111 222</li>
+							<li><i class="fa fa-envelope" aria-hidden="true"></i> trojanhotel@gmail.com</li>
+						</ul>
+					</div><!--con-links end-->
+					<div class="social-links">
+						<ul class="social-lnks">
+							<li><a href="#" title=""><i class=""></i></a></li>
+							<li><a href="#" title=""><i class=""></i></a></li>
+							<li><a href="#" title=""><i class=""></i></a></li>
+						
+						</ul>
+						
+					</div><!--socail-links end-->
+				</div>
+			</div><!--top-bar end-->
+			<div class="bottom-header">
+				<div class="container">
+					<div class="hd">
+						<div class="logo">
+							<a href="#" title="">
+								<img src="images/logo1.png" alt="" />
+							</a>
+						</div><!--logo end-->
+						<div class="menu-search">
+							<nav>
+								<ul>
+									<li class="active menu-item-has-children"><a href="index.php" title=""><i class="fa fa-joomla"></i>Home</a>
+										
+									</li>
+									
+									<li><a href="about.php" title="">Rreth Nesh</a></li>
+									<li><a href="contact.php" title="">Na Kontaktoni</a></li>
+								</ul>
+		 					</nav><!--nav end-->
+							
+						</div><!--menu-search end-->
+						<div class="menu-icon">
+							<span class="first-bar"></span>
+							<span class="second-bar"></span>
+							<span class="third-bar"></span>
+						</div>
+					</div>
+				</div>
+			</div><!--bottom-header end-->
+		</header><!--Header end-->
+	
+	
+		<div class="about-bg">
+			<div class="container">
+				<div class="rl-banner"></br>
+					<h2 class="tp">Rreth Nesh</h2>
+					<ul></br>
+						<li><a href="#">Home</a></li>
+						<li><span class="active">Rreth Nesh</span></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	
+
+
+		<section>
+			<div class="pd90">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7">
+							<div class="relax-history">
+								<h3>Trojan Hotel - Nga 1995</h3>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip .</p>
+								<p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.</p>
+								<ul>
+									<li>
+										<h5>200</h5>
+										<span>Dhoma</span>
+									</li>
+									<li>
+										<h5>75</h5>
+										<span>Stafi</span>
+									</li>
+									<li>
+										<h5>8</h5>
+										<span>Pishina</span>
+									</li>
+									<li>
+										<h5>5</h5>
+										<span>Restaurante</span>
+									</li>
+								</ul>
+							</div><!--relax-history end-->
+						</div>
+						<div class="col-md-5">
+							<div class="history-carousel">
+								<div class="history-img">
+									<img src="http://creativethemes.us/relax/images/resources/history1.jpg" alt="">
+								</div><!--history-img end-->
+								<div class="history-img">
+									<img src="http://creativethemes.us/relax/images/resources/history1.jpg" alt="">
+								</div><!--history-img end-->
+							</div><!--history-carousel end-->
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<section>
+			<div class="container">
+				<div class="about-imgs">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="abt-img">
+								<img src="http://creativethemes.us/relax/images/resources/about1.jpg" alt="">
+							</div><!--abt-img end-->
+						</div>
+						<div class="col-md-3">
+							<div class="abt-img">
+								<img src="http://creativethemes.us/relax/images/resources/about2.jpg" alt="">
+							</div><!--abt-img end-->
+						</div>
+						<div class="col-md-3">
+							<div class="abt-img">
+								<img src="http://creativethemes.us/relax/images/resources/about3.jpg" alt="">
+							</div><!--abt-img end-->
+						</div>
+						<div class="col-md-3">
+							<div class="abt-img">
+								<img src="http://creativethemes.us/relax/images/resources/about4.jpg" alt="">
+							</div><!--abt-img end-->
+						</div>
+					</div>
+				</div><!--about-imgs end-->
+				<div class="abt-links">
+					<div class="row no-gutter">
+						<div class="col-md-6">
+							<ul>
+								<li><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p></li>
+								<li><p>Nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p></li>
+								<li><p>Nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p></li>
+								<li><p>Nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p></li>
+							</ul>
+						</div>
+						<div class="col-md-6">
+							<ul>
+								<li><p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper.</p></li>
+								<li><p>Duis autem vel eum iriure dolor in hendrerit</p></li>
+								<li><p>Blandit praesent luptatum zzril delenit  te feugait nulla facilisi.</p></li>
+								<li><p>Nam liber tempor cum soluta nobis eleifend possim assum</p></li>
+							</ul>
+						</div>
+					</div>
+					
+					
+				</div><!--abt-links end-->
+			</div>
+		</section>	
+		
+	
+	
+
+		<footer>
+			<div class="block no-padding">
+				<div class="bg bg3">
+					<div class="container">
+						<div class="top-footer">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="widget">
+										<div class="about-widget">
+											<a href="#" title=""><img src="images/logo2s.png" alt=""></a>
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,  sed diam nonummy nibh euismod it tincidunt ut laoreet commodo consequat.  </p>
+										
+											<ul class="sc-links">
+												<li><a href="#" title=""><i class=""></i></a></li>
+												<li><a href="#" title=""><i class=""></i></a></li>
+												<li><a href="#" title=""><i class=""></i></a></li>
+											
+											</ul>
+										</div><!--about-widget end-->
+									</div><!--widget end-->
+								</div>
+								<div class="col-md-3">
+									<div class="widget">
+										<div class="news-widget">
+											<h3 class="widget-title">Dhomat</h3>
+											<div class="news">
+												<img src="images/resources/sroom1.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Single Room</a></h3>
+													<span>$85 një natë</span>
+												</div>
+											</div><!--news end-->
+											<div class="news">
+												<img src="images/resources/sroom2.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Luxury Room</a></h3>
+													<span>$352 një natë</span>
+												</div>
+											</div><!--news end-->
+											<div class="news">
+												<img src="images/resources/sroom3.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Double Room </a></h3>
+													<span>$125 një natë</span>
+												</div>
+											</div><!--news end-->
+										</div><!--news-widget end-->
+									</div><!--widget end-->
+								</div>
+								<div class="col-md-3">
+									<div class="widget">
+										<div class="widget-quick-links">
+											<h3 class="widget-title">Linqet</h3>
+											<div class="qk-links">
+												<ul>
+												<li><a href="#" title="">Home</a></li>
+													<li><a href="#" title="">Rreth nesh</a></li>
+													<li><a href="#" title="">Suitat</a></li>
+													<li><a href="#" title="">Lajmet</a></li>
+													<li><a href="#" title="">Na kontaktoni</a></li>
+													<li><a href="#" title="">Rezervimet</a></li>
+												</ul>
+												<ul>
+												<li><a href="#" title="">Aktivitetet</a></li>
+													<li><a href="#" title="">Galeria</a></li>
+													<li><a href="#" title="">Ofertat</a></li>
+												</ul>
+											</div>
+										</div><!--widget-quick-links end-->
+									</div><!--widget-end-->
+								</div>
+								<div class="col-md-2 lst">
+									<div class="widget">
+										<div class="widget-tags">
+											<h3 class="widget-title">Tags</h3>
+											<ul>
+											<li><a href="#" title="">Rezervimet</a></li>
+												<li><a href="#" title="">TV</a></li>
+												<li><a href="#" title="">Servisi</a></li>
+												<li><a href="#" title="">Dhomat</a></li>
+												<li><a href="#" title="">Hotelet</a></li>
+												<li><a href="#" title="">WIFI</a></li>
+												<li><a href="#" title="">Kamera</a></li>
+												<li><a href="#" title="">Party</a></li>
+												<li><a href="#" title="">Pishina</a></li>
+												<li><a href="#" title="">Ofertat</a></li>
+											</ul>
+										</div><!--widget-tags end-->
+									</div><!--widget end-->
+								</div>
+							</div>
+						</div><!--top-footer end-->
+						<div class="bottom-footer">
+							<div class="copyright">
+								<p>© 2023 . All Rights Reserved. </p>
+							</div><!--copyright end-->
+							<div class="credit">
+								<p>Punuar nga : <a href="#" title>Edona Gashi, Ardian Sallauka & Ilir Selmani Drita Kryeziu</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+
+
+
+	</div>
+
+<!-- Including Jquery Js File -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/jquery.min.js"></script>
+<!-- Including Bootstrap js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/bootstrap.min.js"></script>
+<!-- Custom Js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/script.js"></script>
+
+
+</body>
+
+
+
+</html>
