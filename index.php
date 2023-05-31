0e82926773d61da44cb2bcf15cc2b0da758f8f65
ardian-sa <ardiansallauka1@gmail.com>
2023-05-31 19:11:17 +0200

    index2

--- index.php
+++ index.php
@@ -303,3 +303,295 @@ if(isset($_GET['search']))
 				</div>
 			</div>
 		</section>	
+		
+		<section>
+			<div class="block remove-btm-gap">
+				<div class="container">ss
+					<div class="row">
+						<div class="col-md-2">
+							<div class="title f3">
+								<h3>Galeria dhomave</h3>
+							</div>
+							<div class="options">
+								<div class="option-isotop">
+									<ul id="filter" class="option-set filters-nav" data-option-key="filter">
+										<li><a class="selected" data-option-value="*">Fotot e dhomave</a></li>
+									
+									</ul>
+								</div>
+							</div><!-- FILTER BUTTONS -->
+						</div>
+						<div class="col-md-10">
+							<div class="row gallery grid">
+								<div class="col-md-5 col-sm-5 col-xs-5 rooms swimming">
+									<div class="grid-item2 width-auto">
+										<figure>
+											<img src="images/resources/01.jpg" alt="">
+											<figcaption>
+												<h5>Bed room</h5>
+												<ul>
+													<li><a href="#" title=""><i class="fa fa-television"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-wifi"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-video-camera"></i></a></li>
+												</ul>
+											</figcaption>
+											<div class="popup-icon">
+												<a class="html5lightbox" data-thumbnail="images/resources/gallery1.jpg" data-group="set1" href="images/resources/gallery1.jpg" title="home 1"><i class="fa fa-compress"></i></a>
+											</div>
+										</figure>
+									</div>
+								</div>
+
+
+								<div class=" col-md-2 col-sm-2 col-xs-2  kitchen">
+									<div class="grid-item2 width-auto">
+										<figure>
+											<img src="images/resources/02.jpg" alt="">
+											<figcaption>
+												<h5>Bed room</h5>
+												<ul>
+													<li><a href="#" title=""><i class="fa fa-television"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-wifi"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-video-camera"></i></a></li>
+												</ul>
+											</figcaption>
+											<div class="popup-icon">
+												<a class="html5lightbox" data-thumbnail="images/resources/gallery2.jpg" data-group="set1" href="images/resources/gallery2.jpg" title="home 2"><i class="fa fa-compress"></i></a>
+											</div>
+										</figure>
+									</div>
+								</div>
+
+
+								<div class="col-md-5 col-sm-5 col-xs-5 dinning bath">
+									<div class="grid-item2 width-auto">
+										<figure>
+											<img src="images/resources/03.jpg" alt="">
+											<figcaption>
+												<h5>Bed room</h5>
+												<ul>
+													<li><a href="#" title=""><i class="fa fa-television"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-wifi"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-video-camera"></i></a></li>
+												</ul>
+											</figcaption>
+											<div class="popup-icon">
+												<a class="html5lightbox" data-thumbnail="images/resources/gallery3.jpg" data-group="set1" href="images/resources/gallery3.jpg" title="home 3"><i class="fa fa-compress"></i></a>
+											</div>
+										</figure>
+									</div>
+								</div>
+
+
+
+								<div class="col-md-5  col-sm-5 col-xs-5  bath rooms">
+									<div class="grid-item2 width-auto">
+										<figure>
+											<img src="images/resources/04.jpg" alt="">
+											<figcaption>
+												<h5>Bed room</h5>
+												<ul>
+													<li><a href="#" title=""><i class="fa fa-television"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-wifi"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-video-camera"></i></a></li>
+												</ul>
+											</figcaption>
+											<div class="popup-icon">
+												<a class="html5lightbox" data-thumbnail="images/resources/gallery4.jpg" data-group="set1" href="images/resources/gallery4.jpg" title="home 4"><i class="fa fa-compress"></i></a>
+											</div>
+										</figure>
+									</div>
+								</div>
+
+
+
+								<div class=" col-md-7 col-sm-7 col-xs-7  swimming kitchen">
+									<div class="grid-item2 width-auto">
+										<figure>
+											<img src="images/resources/05.jpg" alt="">
+											<figcaption>
+												<h5>Bed room</h5>
+												<ul>
+													<li><a href="#" title=""><i class="fa fa-television"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-wifi"></i></a></li>
+													<li><a href="#" title=""><i class="fa fa-video-camera"></i></a></li>
+												</ul>
+											</figcaption>
+											<div class="popup-icon">
+												<a class="html5lightbox" data-thumbnail="images/resources/gallery5.jpg" data-group="set1" href="images/resources/gallery5.jpg" title="home 5"><i class="fa fa-compress"></i></a>
+											</div>
+										</figure>
+									</div>
+								</div>
+
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section>
+			<div class="pd-btm-less">
+				<div class="container">
+					<div class="partners-logo">
+						
+					</div><!--partners-logo end-->
+				</div>
+			</div>
+		</section>
+
+		
+
+
+
+		<section>
+			<div class="block no-padding">
+				<div class="newsletter">
+					<div class="bg bg2">
+						<div class="container">
+							<div class="stay-tuned">
+								<h2>Qëndroni me ne</h2>
+								<h5>Përjetoni ofertat tona, zbritje, evente dhe shumë gjëra tjera!</h5>
+							</div>
+							<div class="email-form">
+								<form>
+									<input name="" placeholder="Enter your email address" type="text">
+									<button type="submit">Subscribe</button>
+								</form>
+							</div><!--email-form end-->
+						</div>
+					</div>
+				</div><!--newsletter end-->
+			</div>
+		</section>
+
+		<footer>
+			<div class="block no-padding">
+				<div class="bg bg3">
+					<div class="container">
+						<div class="top-footer">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="widget">
+										<div class="about-widget">
+											<a href="#" title=""><img src="images/logo2s.png" alt=""></a>
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,  sed diam nonummy nibh euismod it tincidunt ut laoreet commodo consequat.  </p>
+										
+											<ul class="sc-links">
+												<li><a href="#" title=""><i class=""></i></a></li>
+												<li><a href="#" title=""><i class=""></i></a></li>
+												<li><a href="#" title=""><i class=""></i></a></li>
+											
+											</ul>
+										</div><!--about-widget end-->
+									</div><!--widget end-->
+								</div>
+								<div class="col-md-3">
+									<div class="widget">
+										<div class="news-widget">
+											<h3 class="widget-title">Dhomat</h3>
+											<div class="news">
+												<img src="images/resources/sroom1.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Single Room</a></h3>
+													<span>$85 një natë</span>
+												</div>
+											</div><!--news end-->
+											<div class="news">
+												<img src="images/resources/sroom2.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Luxury Room</a></h3>
+													<span>$352 një natë</span>
+												</div>
+											</div><!--news end-->
+											<div class="news">
+												<img src="images/resources/sroom3.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Double Room </a></h3>
+													<span>$125 një natë</span>
+												</div>
+											</div><!--news end-->
+										</div><!--news-widget end-->
+									</div><!--widget end-->
+								</div>
+								<div class="col-md-3">
+									<div class="widget">
+										<div class="widget-quick-links">
+											<h3 class="widget-title">Linqet</h3>
+											<div class="qk-links">
+												<ul>
+													<li><a href="#" title="">Home</a></li>
+													<li><a href="#" title="">Rreth nesh</a></li>
+													<li><a href="#" title="">Suitat</a></li>
+													<li><a href="#" title="">Lajmet</a></li>
+													<li><a href="#" title="">Na kontaktoni</a></li>
+													<li><a href="#" title="">Rezervimet</a></li>
+													
+												</ul>
+												<ul>
+													<li><a href="#" title="">Aktivitetet</a></li>
+													<li><a href="#" title="">Galeria</a></li>
+													<li><a href="#" title="">Ofertat</a></li>
+												
+												</ul>
+											</div>
+										</div><!--widget-quick-links end-->
+									</div><!--widget-end-->
+								</div>
+								<div class="col-md-2 lst">
+									<div class="widget">
+										<div class="widget-tags">
+											<h3 class="widget-title">Tags</h3>
+											<ul>
+												<li><a href="#" title="">Rezervimet</a></li>
+												<li><a href="#" title="">TV</a></li>
+												<li><a href="#" title="">Servisi</a></li>
+												<li><a href="#" title="">Dhomat</a></li>
+												<li><a href="#" title="">Hotelet</a></li>
+												<li><a href="#" title="">WIFI</a></li>
+												<li><a href="#" title="">Kamera</a></li>
+												<li><a href="#" title="">Party</a></li>
+												<li><a href="#" title="">Pishina</a></li>
+												<li><a href="#" title="">Ofertat</a></li>
+											</ul>
+										</div><!--widget-tags end-->
+									</div><!--widget end-->
+								</div>
+							</div>
+						</div><!--top-footer end-->
+						<div class="bottom-footer">
+							<div class="copyright">
+								<p>© 2023 . All Rights Reserved. </p>
+							</div><!--copyright end-->
+							<div class="credit">
+								<p>Punuar nga : <a href="#" title>Edona Gashi, Ardian Sallauka & Ilir Selmani Drita Kryeziu</a></p>
+							</div><!--credit end-->
+						</div><!--bottom-footer end-->
+					</div>
+				</div>
+			</div>
+		</footer><!--footer end-->
+		
+	</div><!--wrapper end-->
+
+<!-- Including Jquery Js File -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<!-- Including Bootstrap js file -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/flatpickr.min.js"></script>
+<script type="text/javascript" src="js/isotope.js"></script>
+<script type="text/javascript" src="js/html5lightbox.js"></script>
+<script type="text/javascript" src="js/wow.js"></script>
+
+<!-- Custom Js file -->
+<script type="text/javascript" src="js/script.js"></script>
+
+
+</body>
+
+
+
+</html>
